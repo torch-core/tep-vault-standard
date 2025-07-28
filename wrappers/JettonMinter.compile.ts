3e340ae07fdb4dd9e18e06e732f4ee0981f7ff1a
ipromise2324 <168allen55@gmail.com>
2025-07-28 23:57:12 +0800

    Remove outdated jetton contract files and introduce new implementations for jetton wallet and minter functionality. This update includes the addition of gas management, error handling, and utility functions, enhancing the overall asset management capabilities. Update compilation configurations to reflect the new contract structure.

--- wrappers/JettonMinter.compile.ts
+++ wrappers/JettonMinter.compile.ts
@@ -1,16 +1,5 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    // lang: 'func',
-    // targets: [
-    //     'params.fc',
-    //     'op-codes.fc',
-    //     'discovery-params.fc',
-    //     'jetton-utils.fc',
-    //     'jetton-minter-discoverable.fc'
-    // ],
-    lang: 'tolk',
-    entrypoint: 'contracts/mock/jetton-minter-contract.tolk',
-    withSrcLineComments: true,
-    withStackComments: true,
+    targets: ['contracts/mock/jetton-minter.fc'],
 };
--- wrappers/JettonWallet.compile.ts
+++ wrappers/JettonWallet.compile.ts
@@ -1,15 +1,5 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    // lang: 'func',
-    // targets: [
-    //     'params.fc',
-    //     'op-codes.fc',
-    //     'jetton-utils.fc',
-    //     'jetton-wallet.fc'
-    // ],
-    lang: 'tolk',
-    entrypoint: 'contracts/jetton/jetton-wallet-contract.tolk',
-    withSrcLineComments: true,
-    withStackComments: true,
+    targets: ['contracts/jetton/jetton-wallet.fc'],
 };
