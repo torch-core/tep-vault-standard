956efb77074fe524c355f572e6816d4e007a70a9
ipromise2324 <168allen55@gmail.com>
2025-10-14 13:24:27 +0800

    Add TestVault contract and associated scripts for deployment and interaction
    - Introduced the TestVault contract with storage management and message handling for counter operations.
    - Added deployment script to initialize TestVault with a random ID and counter.
    - Created increment and reset scripts to interact with the TestVault, allowing counter manipulation.
    - Implemented unit tests for TestVault to validate functionality and error handling for invalid transfer amounts.
    - Configured compilation settings for the TestVault contract to streamline the build process.

---
+++ tests/TestVault.spec.ts
@@ -0,0 +1,60 @@
+import { Blockchain, printTransactionFees, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Cell, toNano } from '@ton/core';
+import { TestVault } from '../wrappers/TestVault';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
+import { VaultErrors } from '../wrappers/constants/error';
+import { Vault } from '../wrappers/Vault';
+
+describe('TestVault', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('TestVault');
+    });
+
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let testVault: SandboxContract<TestVault>;
+    let vault: SandboxContract<Vault>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        testVault = blockchain.openContract(
+            TestVault.createFromConfig(
+                {
+                    id: 0,
+                    counter: 0,
+                },
+                code,
+            ),
+        );
+        vault = blockchain.openContract(Vault.createFromAddress(testVault.address));
+
+        deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await testVault.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: testVault.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should throw ERR_INVALID_TRANSFER_AMOUNT when transfer amount is 0', async () => {
+        const r = await testVault.sendInvalidTransferAmount(deployer.getSender(), {
+            value: toNano('0.05'),
+        });
+        printTransactionFees(r.transactions);
+
+        expect(r.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: testVault.address,
+            success: false,
+            exitCode: VaultErrors.InvalidTransferAmount,
+        });
+    });
+});
---
+++ wrappers/TestVault.ts
@@ -0,0 +1,88 @@
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+
+export type TestVaultConfig = {
+    id: number;
+    counter: number;
+};
+
+export function testVaultConfigToCell(config: TestVaultConfig): Cell {
+    return beginCell().storeUint(config.id, 32).storeUint(config.counter, 32).endCell();
+}
+
+export const Opcodes = {
+    OP_INCREASE: 0x7e8764ef,
+    OP_INVALID_TRANSFER_AMOUNT: 0xa2519c55,
+};
+
+export class TestVault implements Contract {
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
+
+    static createFromAddress(address: Address) {
+        return new TestVault(address);
+    }
+
+    static createFromConfig(config: TestVaultConfig, code: Cell, workchain = 0) {
+        const data = testVaultConfigToCell(config);
+        const init = { code, data };
+        return new TestVault(contractAddress(workchain, init), init);
+    }
+
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
+
+    async sendIncrease(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            increaseBy: number;
+            value: bigint;
+            queryID?: number;
+        },
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.OP_INCREASE, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .storeUint(opts.increaseBy, 32)
+                .endCell(),
+        });
+    }
+
+    async sendInvalidTransferAmount(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+            queryID?: number;
+        },
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell()
+                .storeUint(Opcodes.OP_INVALID_TRANSFER_AMOUNT, 32)
+                .storeUint(opts.queryID ?? 0, 64)
+                .endCell(),
+        });
+    }
+
+    async getCounter(provider: ContractProvider) {
+        const result = await provider.get('currentCounter', []);
+        return result.stack.readNumber();
+    }
+
+    async getID(provider: ContractProvider) {
+        const result = await provider.get('initialId', []);
+        return result.stack.readNumber();
+    }
+}
